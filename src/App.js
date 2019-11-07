c4cc8b8d449a42350a5b63bdeb7279468552aeb7
Luis Manuel Rodríguez <lrodriguez@mardom.com>
2019-11-07 09:57:26 -0400

    Add setting and timer components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import React from "react";
 import logo from "./logo.svg";
 
+
+function Settings(props) {
+	return (
+		<div>
+			<div>
+				<span id="break-label">Break Length</span>
+				<button id="break-increment" type="button"> + </button>
+				<span id="break-length">{props.breakLength}</span>
+				<button id="break-decrement" type="button"> - </button>
+			</div>
+			<div>
+				<span id="session-label">Session Length</span>
+				<button id="session-increment" type="button"> + </button>
+				<span id="session-length">{props.sessionLength}</span>
+				<button id="session-decrement" type="button"> - </button>
+			</div>
+		</div>
+	);
+}
+
+function Timer() {
+	return (
+		<div>
+			<div>
+				<span id="timer-label">Session</span>
+			</div>
+			<div>
+				<span id="time-left">25:00</span>
+			</div>
+			<div>
+				<button id="start_stop" type="button"> Start </button>
+				<button id="reset" type="button"> Reset </button>
+			</div>
+		</div>
+	);
+}
+
 function App() {
-  return <div>Pomodoro clock</div>;
+	return (
+		<div>
+			<Timer />
+			<Settings breakLength="5" sessionLength="25" />
+		</div>
+	);
 }
 
 export default App;
